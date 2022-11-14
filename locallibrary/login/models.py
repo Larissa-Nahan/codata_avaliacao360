90656b1c88f3b83e3ca6610cbca1381af6ea0ecd
Larissa-Nahan <larissanahan@gmail.com>
2022-11-14 10:56:00 -0300

    recuperar senha template

--- locallibrary/login/models.py
+++ locallibrary/login/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
-# Create your models here.
+from email.policy import default
+from django.db import models   
+    
+class Login(models.Model):
+    
+    senha = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    cpf = models.CharField(max_length=14, blank=False, null=False)
--- locallibrary/login/urls.py
+++ locallibrary/login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('recuperar_senha/', views.recuperar_senha, name='recuperar_senha'),
 ]
