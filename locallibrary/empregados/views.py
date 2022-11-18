0bd1b353b7933c7a4ba59dac29d1b683890352a8
Larissa-Nahan <larissanahan@gmail.com>
2022-11-18 07:51:11 -0300

    mudanca hierarquia de templates

--- locallibrary/empregados/views.py
+++ locallibrary/empregados/views.py
@@ -3,7 +3,7 @@ from recursos_humanos.models import Usuario
 from django.shortcuts import get_object_or_404
 
 def home(request):
-    return render(request, "empregados/home.html")
+    return render(request, "home_usuarios.html")
 
 def auto(request):
     return render(request, 'empregados/auto.html')
